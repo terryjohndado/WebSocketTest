a7d014960039d0c8ec0e407aac3e521c7703b4d2
teejay <167974934+terryjohndado@users.noreply.github.com>
2025-07-16 08:54:53 +0900

    Add project files.

---
+++ ApiResponse.cs
@@ -0,0 +1,218 @@
+﻿// To parse this JSON data, add NuGet 'Newtonsoft.Json' then do:
+//
+//    using ApiResponse;
+//
+//    var bsEvent = BsEvent.FromJson(jsonString);
+
+namespace ApiResponse
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class BsEvent
+    {
+        [JsonProperty("Event", Required = Required.Always)]
+        public virtual Event Event { get; set; }
+    }
+
+    public partial class Event
+    {
+        [JsonProperty("id")]
+        [JsonConverter(typeof(PurpleParseStringConverter))]
+        public virtual long Id { get; set; }
+
+        [JsonProperty("event_type_id")]
+        public virtual EventTypeId EventTypeId { get; set; }
+
+        [JsonProperty("index")]
+        [JsonConverter(typeof(PurpleParseStringConverter))]
+        public virtual long Index { get; set; }
+
+        [JsonProperty("datetime")]
+        public virtual DateTimeOffset Datetime { get; set; }
+
+        [JsonProperty("server_datetime")]
+        public virtual DateTimeOffset ServerDatetime { get; set; }
+
+        [JsonProperty("device_id")]
+        public virtual DeviceId DeviceId { get; set; }
+
+        [JsonProperty("user_id")]
+        public virtual UserId UserId { get; set; }
+
+        [JsonProperty("tna_key")]
+        [JsonConverter(typeof(PurpleParseStringConverter))]
+        public virtual long TnaKey { get; set; }
+
+        [JsonProperty("parameter")]
+        [JsonConverter(typeof(PurpleParseStringConverter))]
+        public virtual long Parameter { get; set; }
+
+        [JsonProperty("is_dst")]
+        [JsonConverter(typeof(PurpleParseStringConverter))]
+        public virtual long IsDst { get; set; }
+
+        [JsonProperty("timezone")]
+        public virtual Timezone Timezone { get; set; }
+
+        [JsonProperty("temperature")]
+        [JsonConverter(typeof(PurpleParseStringConverter))]
+        public virtual long Temperature { get; set; }
+    }
+
+    public partial class DeviceId
+    {
+        [JsonProperty("id")]
+        [JsonConverter(typeof(PurpleParseStringConverter))]
+        public virtual long Id { get; set; }
+
+        [JsonProperty("name")]
+        public virtual string Name { get; set; }
+    }
+
+    public partial class EventTypeId
+    {
+        [JsonProperty("code")]
+        [JsonConverter(typeof(PurpleParseStringConverter))]
+        public virtual long Code { get; set; }
+
+        [JsonProperty("name")]
+        public virtual string Name { get; set; }
+
+        [JsonProperty("description")]
+        public virtual string Description { get; set; }
+
+        [JsonProperty("enable_alert")]
+        [JsonConverter(typeof(FluffyParseStringConverter))]
+        public virtual bool EnableAlert { get; set; }
+
+        [JsonProperty("alertable")]
+        [JsonConverter(typeof(FluffyParseStringConverter))]
+        public virtual bool Alertable { get; set; }
+
+        [JsonProperty("alert_name")]
+        public virtual string AlertName { get; set; }
+
+        [JsonProperty("alert_message")]
+        public virtual string AlertMessage { get; set; }
+    }
+
+    public partial class Timezone
+    {
+        [JsonProperty("half")]
+        [JsonConverter(typeof(PurpleParseStringConverter))]
+        public virtual long Half { get; set; }
+
+        [JsonProperty("hour")]
+        [JsonConverter(typeof(PurpleParseStringConverter))]
+        public virtual long Hour { get; set; }
+
+        [JsonProperty("negative")]
+        [JsonConverter(typeof(PurpleParseStringConverter))]
+        public virtual long Negative { get; set; }
+    }
+
+    public partial class UserId
+    {
+        [JsonProperty("user_id")]
+        [JsonConverter(typeof(PurpleParseStringConverter))]
+        public virtual long UserIdUserId { get; set; }
+
+        [JsonProperty("name")]
+        public virtual string Name { get; set; }
+
+        [JsonProperty("photo_exists")]
+        [JsonConverter(typeof(FluffyParseStringConverter))]
+        public virtual bool PhotoExists { get; set; }
+    }
+
+    public partial class BsEvent
+    {
+        public static BsEvent FromJson(string json) => JsonConvert.DeserializeObject<BsEvent>(json, ApiResponse.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this BsEvent self) => JsonConvert.SerializeObject(self, ApiResponse.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+
+    internal class PurpleParseStringConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(long) || t == typeof(long?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            long l;
+            if (Int64.TryParse(value, out l))
+            {
+                return l;
+            }
+            throw new Exception("Cannot unmarshal type long");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (long)untypedValue;
+            serializer.Serialize(writer, value.ToString());
+            return;
+        }
+
+        public static readonly PurpleParseStringConverter Singleton = new PurpleParseStringConverter();
+    }
+
+    internal class FluffyParseStringConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(bool) || t == typeof(bool?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            bool b;
+            if (Boolean.TryParse(value, out b))
+            {
+                return b;
+            }
+            throw new Exception("Cannot unmarshal type bool");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (bool)untypedValue;
+            var boolString = value ? "true" : "false";
+            serializer.Serialize(writer, boolString);
+            return;
+        }
+
+        public static readonly FluffyParseStringConverter Singleton = new FluffyParseStringConverter();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using RestSharp;
+
+using ApiResponse;
+
+namespace WebSocketTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BSWebSocket socket = new BSWebSocket();
+            socket.Method();
+        }
+    }
+
+    class BSWebSocket 
+    {
+        static string API_HOST = "https://192.168.103.3:443";
+        static string WS_HOST = "wss://192.168.103.3:443";
+        static string BIOSTAR2_LOGIN_API_URI = API_HOST + "/api/login";
+        static string BIOSTAR2_WS_URI = WS_HOST + "/wsapi";
+        static string BIOSTAR2_WS_EVENT_START_URI = API_HOST + "/api/events/start";
+        static string bsSessionID = "";
+
+        public virtual async void Method()
+        {
+            Task.Run(async () =>
+            {
+                await ConnectWebSocket();
+            }).Wait();
+        }
+
+        async Task ConnectWebSocket()
+        {
+            try
+            {
+                //Set login data
+                var loginData = new
+                {
+                    User = new
+                    {
+                        login_id = "admin",
+                        password = "Suprema!1"
+                    }
+                };
+
+                //Set SSH certificates
+                System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+                System.Net.ServicePointManager.Expect100Continue = false;
+                ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    //Set up the HttpClient to send the login request to the BioStar 2 server
+                    httpClient.BaseAddress = new Uri(API_HOST);
+                    httpClient.DefaultRequestHeaders.Accept.Clear();
+                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    httpClient.Timeout = new TimeSpan(1,1,1);
+                    var httpContent = new StringContent(JsonConvert.SerializeObject(loginData), Encoding.UTF8, "application/json");
+
+                    //Login and get BioStar2 Session ID
+                    HttpResponseMessage httpResponse = await httpClient.PostAsync("api/login", httpContent);
+                    if (httpResponse.IsSuccessStatusCode == true)
+                    {
+                        bsSessionID = httpResponse.Headers.GetValues("bs-session-id").FirstOrDefault().ToString();
+                        Console.WriteLine("bs-session-id: " + bsSessionID);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failed to Login");
+                        Console.WriteLine(httpResponse.ToString());
+                    }
+
+                    //Connect to WebSocket endpoint
+                    var clientWebSocket = new ClientWebSocket();
+                    await clientWebSocket.ConnectAsync(new Uri(BIOSTAR2_WS_URI), CancellationToken.None);
+
+                    //Add the session ID to the websocket connection URI
+                    byte[] buffer = Encoding.UTF8.GetBytes("bs-session-id="+ bsSessionID);
+                    await clientWebSocket.SendAsync(new ArraySegment<byte>(buffer), WebSocketMessageType.Text, true, CancellationToken.None);
+
+                    //Start and Receive Realtime Event Logs
+                    await StartMonitoringEvents();
+                    await ReceiveMonitoringEvents(clientWebSocket);
+                }
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine(x.Message);
+            }
+
+        }
+
+        private async Task StartMonitoringEvents()
+        {
+            var client = new RestClient(BIOSTAR2_WS_EVENT_START_URI);
+            var request = new RestRequest("",RestSharp.Method.Post);
+            request.AddHeader("Accept", "application/json, text/plain, */*");
+            request.AddHeader("content-type", "application/json;charset=UTF-8");
+            request.AddHeader("bs-session-id", bsSessionID);
+            request.AddHeader("content-language", "en");
+            request.AddParameter("application/json", "{\"Query\":{}}", ParameterType.RequestBody);
+            RestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+        }
+
+        private static async Task ReceiveMonitoringEvents(ClientWebSocket webSocket)
+        {
+            byte[] buffer = new byte[1024];
+            while (webSocket.State == WebSocketState.Open)
+            {
+                var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+                }
+                else
+                {
+                    string jsonData = Encoding.UTF8.GetString(buffer);
+                    try
+                    {
+                        var bsEvent = BsEvent.FromJson(jsonData);
+
+                        //Filter by Device ID
+                        if (bsEvent.Event.DeviceId.Id == 538151189)
+                        {
+                            Console.WriteLine(jsonData);
+                        }
+
+                        //Filter by Event ID
+                        if(bsEvent.Event.EventTypeId.Code == 4102) // 4102 = VERIFY_SUCCESS_CARD, replace with target events
+                        {
+                            Console.WriteLine(jsonData);
+                        }
+                    }
+                    catch(Exception x)
+                    {
+                        Console.WriteLine(x.Message);
+                    }
+                }
+            }
+        }
+    }
+}
